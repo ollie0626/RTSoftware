f28c09ce4aea20ab12b00bfcecb77490ec20d285
ollie_lin <ollie_lin@richtek.com>
2022-07-13 16:42:45 +0800

    update

---
+++ BuckTool/ATE_Eff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace BuckTool
+{
+    public class ATE_Eff : ITask
+    {
+        Excel.Application _app;
+        Excel.Worksheet _sheet;
+        Excel.Workbook _book;
+        Excel.Range _range;
+
+        public void ATE_Task()
+        {
+
+        }
+    }
+}
