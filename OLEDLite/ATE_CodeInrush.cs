bb90f4a45c2934dc98aeb8163dea732a28385131
ollie_lin <westgate.skater@gmail.com>
2022-11-21 17:21:54 +0800

    update change code

--- OLEDLite/ATE_CodeInrush.cs
+++ OLEDLite/ATE_CodeInrush.cs
@@ -64,10 +64,12 @@ namespace OLEDLite
             InsControl._scope.TimeScaleMs(test_parameter.ontime_scale_ms);
             System.Threading.Thread.Sleep(1000);
 
-            System.Threading.Thread.Sleep(1000);
+            //System.Threading.Thread.Sleep(1000);
             double trigger_level = neg_vol ? (level_max * 0.8) * -1 : level_max * 0.8;
             InsControl._scope.TriggerLevel_CH1(trigger_level);
             System.Threading.Thread.Sleep(500);
+            //InsControl._scope.Trigger_CH2();
+            //InsControl._scope.TriggerLevel_CH2(1.2);
 
             InsControl._scope.DoCommand(":MEASure:THResholds:RFALl:METHod ALL,PERCent");
             InsControl._scope.DoCommand(":MEASure:THResholds:RFALl:PERCent ALL,100,50,0");
@@ -90,54 +92,50 @@ namespace OLEDLite
             string[] binList = new string[1];
             binList = MyLib.ListBinFile(test_parameter.bin_path);
             bin_cnt = binList.Length;
-            bool ispos = Math.Abs(test_parameter.vol_max) > Math.Abs(test_parameter.vol_min);
+            bool ispos = test_parameter.vol_max > test_parameter.vol_min;
             int vin_cnt = test_parameter.vinList.Count;
             int iout_cnt = test_parameter.ioutList.Count;
             double[] ori_vinTable = new double[vin_cnt];
             Array.Copy(test_parameter.vinList.ToArray(), ori_vinTable, vin_cnt);
 
             RTDev.BoadInit();
+#if Report
+            //MyLib.ExcelReportInit(_sheet);
+            //MyLib.testCondition(_sheet, "Code Inrush", bin_cnt, temp);
+            _app = new Excel.Application();
+            _app.Visible = true;
+            _book = (Excel.Workbook)_app.Workbooks.Add();
+            _sheet = (Excel.Worksheet)_book.ActiveSheet;
+            _sheet.Cells[row, XLS_Table.A] = "No.";
+            _sheet.Cells[row, XLS_Table.B] = "Temp(C)";
+            _sheet.Cells[row, XLS_Table.C] = "Vin(V)";
+            _sheet.Cells[row, XLS_Table.D] = "Iin(mA)";
+            _sheet.Cells[row, XLS_Table.E] = test_parameter.i2c_enable ? "Bin File" : "Swire";
+            _sheet.Cells[row, XLS_Table.F] = "Iout (mA)";
+            _sheet.Cells[row, XLS_Table.G] = "Imax(mA)_min";
+            _sheet.Cells[row, XLS_Table.H] = "Vmax(V)_min";
+            _sheet.Cells[row, XLS_Table.I] = "Vmin(V)_min";
+            _sheet.Cells[row, XLS_Table.J] = "Imax(mA)_max";
+            _sheet.Cells[row, XLS_Table.K] = "Vmax(V)_max";
+            _sheet.Cells[row, XLS_Table.L] = "Vmin(V)_max";
+            _range = _sheet.Range["A" + row, "L" + row];
+            _range.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            _range = _sheet.Range["A" + row.ToString(), "F" + row.ToString()];
+            _range.Interior.Color = Color.FromArgb(124, 252, 0);
+
+            _range = _sheet.Range["G" + row.ToString(), "L" + row.ToString()];
+            _range.Interior.Color = Color.FromArgb(30, 144, 255);
+            row++;
+#endif
 
             OSCInit();
             InsControl._power.AutoPowerOff();
 
             for (int bin_idx = 0; bin_idx < (test_parameter.i2c_enable ? bin_cnt : test_parameter.swireList.Count); bin_idx++)
             {
-#if Report
-                _app = new Excel.Application();
-                _app.Visible = true;
-                _book = (Excel.Workbook)_app.Workbooks.Add();
-                _sheet = (Excel.Worksheet)_book.ActiveSheet;
-#endif
-
                 for (int vin_idx = 0; vin_idx < vin_cnt; vin_idx++)
                 {
-#if Report
-                    //MyLib.ExcelReportInit(_sheet);
-                    //MyLib.testCondition(_sheet, "Code Inrush", bin_cnt, temp);
-                    _sheet.Cells[row, XLS_Table.A] = "No.";
-                    _sheet.Cells[row, XLS_Table.B] = "Temp(C)";
-                    _sheet.Cells[row, XLS_Table.C] = "Vin(V)";
-                    _sheet.Cells[row, XLS_Table.D] = "Iin(mA)";
-                    _sheet.Cells[row, XLS_Table.E] = test_parameter.i2c_enable ? "Bin File" : "Swire";
-                    _sheet.Cells[row, XLS_Table.F] = "Imax(mA)_min";
-                    _sheet.Cells[row, XLS_Table.G] = "Vmax(V)_min";
-                    _sheet.Cells[row, XLS_Table.H] = "Vmin(V)_min";
-                    _sheet.Cells[row, XLS_Table.I] = "Imax(mA)_max";
-                    _sheet.Cells[row, XLS_Table.J] = "Vmax(V)_max";
-                    _sheet.Cells[row, XLS_Table.K] = "Vmin(V)_max";
-                    _range = _sheet.Range["A" + row, "K" + row];
-                    _range.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
-
-                    _range = _sheet.Range["A" + row.ToString(), "E" + row.ToString()];
-                    _range.Interior.Color = Color.FromArgb(124, 252, 0);
-
-                    _range = _sheet.Range["F" + row.ToString(), "K" + row.ToString()];
-                    _range.Interior.Color = Color.FromArgb(30, 144, 255);
-                    row++;
-#endif
-                    start_pos.Add(row);
-
                     for (int iout_idx = 0; iout_idx < iout_cnt; iout_idx++)
                     {
                         if (test_parameter.run_stop == true) goto Stop;
@@ -158,29 +156,36 @@ namespace OLEDLite
                             System.Windows.Forms.MessageBox.Show("34970 沒有連結 !!", "ATE Tool", System.Windows.Forms.MessageBoxButtons.OK);
                             return;
                         }
-                        //if (test_parameter.specify_bin != "") RTDev.I2C_WriteBin((byte)(test_parameter.specify_id >> 1), 0x00, test_parameter.specify_bin);
+                        
                         if (binList[0] != "" && test_parameter.i2c_enable) RTDev.I2C_WriteBin((byte)(test_parameter.slave >> 1), 0x00, binList[bin_idx]);
+                        else
+                        {
+                            // ic setting
+                            int[] pulse_tmp = test_parameter.swireList[bin_idx].Split(',').Select(int.Parse).ToArray();
+                            for (int pulse_idx = 0; pulse_idx < pulse_tmp.Length; pulse_idx++) RTDev.SwirePulse(pulse_tmp[pulse_idx]);
+                        }
 
                         /* test conditonss */
                         byte[] buf_min = new byte[1] { (byte)test_parameter.code_min };
                         byte[] buf_max = new byte[1] { (byte)test_parameter.code_max };
 
 
-                        double max, min, vin, iin, imax;
+                        double max, min, vin, iin, imax, iout;
                         vin = InsControl._34970A.Get_100Vol(1);
                         iin = InsControl._power.GetCurrent();
+                        iout = InsControl._eload.GetIout() * 1000;
 #if Report
                         _sheet.Cells[row, XLS_Table.A] = idx;
                         _sheet.Cells[row, XLS_Table.B] = temp;
                         _sheet.Cells[row, XLS_Table.C] = vin;
-                        _sheet.Cells[row, XLS_Table.D] = iin;
-                        _sheet.Cells[row, XLS_Table.E] = test_parameter.i2c_enable ? Path.GetFileNameWithoutExtension(binList[bin_idx]) : test_parameter.code_min + "→" + test_parameter.code_max;
+                        _sheet.Cells[row, XLS_Table.D] = iin * 1000;
+                        _sheet.Cells[row, XLS_Table.E] = test_parameter.i2c_enable ? Path.GetFileNameWithoutExtension(binList[bin_idx]) : "setting: " + test_parameter.swireList[bin_idx] + "_Channel pulse: " + test_parameter.code_min + "→" + test_parameter.code_max;
+                        _sheet.Cells[row, XLS_Table.F] = iout;
 #endif
                         /* min to max code */
                         InsControl._scope.Root_RUN();
-                        InsControl._scope.SetTrigModeEdge(false);
-                        //if (ispos) 
-                        //else InsControl._scope.SetTrigModeEdge(true);
+                        if (ispos) InsControl._scope.SetTrigModeEdge(false);
+                        else InsControl._scope.SetTrigModeEdge(true);
                         InsControl._scope.NormalTrigger();
 
                         if (test_parameter.i2c_enable)
@@ -209,9 +214,9 @@ namespace OLEDLite
                         max = InsControl._scope.Meas_CH1MAX();
                         min = InsControl._scope.Meas_CH1MIN();
 #if Report
-                        _sheet.Cells[row, XLS_Table.F] = imax * 1000;
-                        _sheet.Cells[row, XLS_Table.G] = max;
-                        _sheet.Cells[row, XLS_Table.H] = min;
+                        _sheet.Cells[row, XLS_Table.G] = imax * 1000;
+                        _sheet.Cells[row, XLS_Table.H] = max;
+                        _sheet.Cells[row, XLS_Table.I] = min;
 #endif
                         InsControl._scope.Root_Clear();
                         System.Threading.Thread.Sleep(2000);
@@ -238,10 +243,10 @@ namespace OLEDLite
                         max = InsControl._scope.Meas_CH1MAX();
                         min = InsControl._scope.Meas_CH1MIN();
 #if Report
-                        _sheet.Cells[row, XLS_Table.I] = imax * 1000;
-                        _sheet.Cells[row, XLS_Table.J] = max;
-                        _sheet.Cells[row, XLS_Table.K] = min;
-                        for (int i = 1; i < 11; i++) _sheet.Columns[i].AutoFit();
+                        _sheet.Cells[row, XLS_Table.J] = imax * 1000;
+                        _sheet.Cells[row, XLS_Table.K] = max;
+                        _sheet.Cells[row, XLS_Table.L] = min;
+                        for (int i = 1; i < 13; i++) _sheet.Columns[i].AutoFit();
 #endif
                         InsControl._scope.Root_Clear();
                         InsControl._power.AutoPowerOff();
