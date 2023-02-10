fa13bf0e1d75fca1604f58ca595756cf504752e4
ollie_lin <westgate.skater@gmail.com>
2023-02-10 17:27:47 +0800

    update sst tool version number

--- SoftStartTiming/AppManager.cs
+++ SoftStartTiming/AppManager.cs
@@ -5,7 +5,7 @@ namespace SoftStartTiming
 {
     public partial class SoftStartTiming
     {
-        private string win_name = "Soft start v1.1";
+        private string win_name = "Soft start v1.2";
 
 
         public CheckBox[] binTable;
