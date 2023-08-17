3758440c12412e02fd6a46f8fc100c972d67f867
林劍豪 <westgate.skater@gmail.com>
2023-08-17 11:21:11 +0800

    update version number

--- SoftStartTiming/LTLab.cs
+++ SoftStartTiming/LTLab.cs
@@ -21,7 +21,7 @@ namespace SoftStartTiming
     {
 
         System.Collections.Generic.Dictionary<string, string> Device_map = new Dictionary<string, string>();
-        string win_name = "LTLab v1.0";
+        string win_name = "LTLab v1.01";
         ParameterizedThreadStart p_thread;
         ATE_LTLab _ate_ltlab;
         Thread ATETask;
